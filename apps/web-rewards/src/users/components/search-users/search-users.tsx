9d847a04d946294f9eb39dda8dea229ea2e205aa
GountzJs <tomasjsdev@gmail.com>
2025-02-14 13:14:37 -0300

    fix(web-rewards): use search users component

--- apps/web-rewards/src/users/components/search-users/search-users.tsx
+++ apps/web-rewards/src/users/components/search-users/search-users.tsx
@@ -1,24 +1,52 @@
+import { useSearchUsers } from '@/users/hooks/use-search-users.hook';
 import { Dropdown, InputOutline } from '@embeeorg/ui-kit';
 import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import styles from './search-users.module.css';
 
 export function SearchUsers() {
   const [search, setSearch] = useState<string>('');
+  const { error, data, isLoading } = useSearchUsers({ username: search });
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const navigate = useNavigate();
 
-  const handlerOnSearch = () => {
-    console.log(search);
+  const getError = (): string | undefined => {
+    if (search.length < 3)
+      return 'Ingrese al menos 3 carácteres para iniciar la búsqueda';
+    if (data.length === 0)
+      return 'No se han encontrado usuarios con el nombre de usuario ingresado.';
+    if (error) return error;
+    return undefined;
   };
-
   return (
-    <form style={{ position: 'relative', width: '500px', maxWidth: '80%' }}>
+    <form
+      className={styles.container}
+      onMouseEnter={() => setIsOpen(true)}
+      onMouseLeave={() => setIsOpen(false)}
+    >
       <InputOutline
         placeholder="Buscar por nombre de usuario"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
-      <Dropdown.Box>
-        <Dropdown.Item handlerOption={handlerOnSearch}>Option 1</Dropdown.Item>
-        <Dropdown.Item handlerOption={handlerOnSearch}>Option 2</Dropdown.Item>
-        <Dropdown.Item handlerOption={handlerOnSearch}>Option 3</Dropdown.Item>
+      <Dropdown.Box isOpen={isOpen} error={getError()} isLoading={isLoading}>
+        {data.map(({ id, username, avatar }) => (
+          <Dropdown.Item
+            key={id}
+            className={styles['item-option']}
+            onClick={() => navigate(`/user/${id.replace('?', '')}`)}
+          >
+            <img
+              className={styles.avatar}
+              src={avatar}
+              loading="lazy"
+              alt={`Avatar ${username}`}
+              width={30}
+              height={30}
+            />
+            {username}
+          </Dropdown.Item>
+        ))}
       </Dropdown.Box>
     </form>
   );
