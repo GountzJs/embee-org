6f425b0c9266dd0d72904f18e1c2756326ffb7c5
GountzJs <tomasjsdev@gmail.com>
2025-02-15 13:38:34 -0300

    feat(web-rewards): listborders add default sort by rank

--- apps/web-rewards/src/borders/components/list-borders/list-borders.tsx
+++ apps/web-rewards/src/borders/components/list-borders/list-borders.tsx
@@ -1,4 +1,5 @@
 import { useBordersUserHook } from '@/borders/hooks/use-borders-user.hook';
+import { UserBorderEntity } from '@/borders/models/entities/user-border.entity';
 import { Rank } from '@/ranking/models/enums/rank.enum';
 import { quantityBorderToRank } from '@/ranking/utils/quantity-border-to-rank';
 import { BorderRank } from '../border-rank/border-rank';
@@ -15,18 +16,26 @@ export function ListBorders({ id }: Props) {
 
   if (error) return <div>Error: {error}</div>;
 
+  const callbackSort = (a: UserBorderEntity, b: UserBorderEntity) => {
+    if (a.special === b.special) return b.quantity - a.quantity;
+
+    return a.special ? -1 : 1;
+  };
+
   return (
     <div className={styles.container}>
-      {data?.map(({ id, url, special, quantity, username, avatar, name }) => (
-        <BorderRank
-          key={id}
-          name={name}
-          rank={special ? Rank.Challenger : quantityBorderToRank(quantity)}
-          url={url}
-          username={username}
-          avatarUrl={avatar}
-        />
-      ))}
+      {data
+        ?.sort(callbackSort)
+        ?.map(({ id, url, special, quantity, username, avatar, name }) => (
+          <BorderRank
+            key={id}
+            name={name}
+            rank={special ? Rank.Challenger : quantityBorderToRank(quantity)}
+            url={url}
+            username={username}
+            avatarUrl={avatar}
+          />
+        ))}
     </div>
   );
 }
