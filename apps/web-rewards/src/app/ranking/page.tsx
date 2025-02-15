936f5eff990f96db8aac8c2c20ed7ff724385413
GountzJs <tomasjsdev@gmail.com>
2025-02-15 13:38:06 -0300

    feat(web-rewards): add info searcher list ranking into ranking page

--- apps/web-rewards/src/app/ranking/page.tsx
+++ apps/web-rewards/src/app/ranking/page.tsx
@@ -1,13 +1,33 @@
 import { ListRanking } from '@/ranking/components/list-ranking/list-ranking';
 import { SearchUsers } from '@/users/components/search-users/search-users';
+import { Typography } from '@embeeorg/ui-kit';
 import styles from './page.module.css';
 
 export default function RankingPage() {
   return (
     <main className={styles.container}>
       <section className={styles['section-container']}>
+        <Typography
+          variant="h4"
+          family="primary"
+          color="white"
+          size="xl"
+          weight="extrabold"
+        >
+          Ingrese su nombre de usuario para ver las recompensas canjeadas
+        </Typography>
         <SearchUsers />
       </section>
+      <Typography
+        variant="h2"
+        family="primary"
+        color="white"
+        size="4xl"
+        weight="extrabold"
+        className={styles['title-page']}
+      >
+        Nuestros usuarios con más bordes
+      </Typography>
       <ListRanking />
     </main>
   );
