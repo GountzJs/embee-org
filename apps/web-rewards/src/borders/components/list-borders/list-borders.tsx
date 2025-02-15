39fe18220d77637fc09ea19688aa152188c0768b
GountzJs <tomasjsdev@gmail.com>
2025-02-15 14:09:47 -0300

    feat(web-rewards): list borders add input search

--- apps/web-rewards/src/borders/components/list-borders/list-borders.tsx
+++ apps/web-rewards/src/borders/components/list-borders/list-borders.tsx
@@ -2,6 +2,8 @@ import { useBordersUserHook } from '@/borders/hooks/use-borders-user.hook';
 import { UserBorderEntity } from '@/borders/models/entities/user-border.entity';
 import { Rank } from '@/ranking/models/enums/rank.enum';
 import { quantityBorderToRank } from '@/ranking/utils/quantity-border-to-rank';
+import { InputOutline, LensSvg, Typography } from '@embeeorg/ui-kit';
+import { useState } from 'react';
 import { BorderRank } from '../border-rank/border-rank';
 import styles from './list-borders.module.css';
 
@@ -10,7 +12,12 @@ interface Props {
 }
 
 export function ListBorders({ id }: Props) {
+  const [search, setSearch] = useState<string>('');
   const { data, isLoading, error } = useBordersUserHook({ id });
+  const borders = data?.filter((border) =>
+    border.name.toLowerCase().includes(search.toLowerCase()),
+  );
+  const [isTouched, setIsTouched] = useState<boolean>(false);
 
   if (isLoading) return <div>Buscando bordes...</div>;
 
@@ -24,18 +31,54 @@ export function ListBorders({ id }: Props) {
 
   return (
     <div className={styles.container}>
-      {data
-        ?.sort(callbackSort)
-        ?.map(({ id, url, special, quantity, username, avatar, name }) => (
-          <BorderRank
-            key={id}
-            name={name}
-            rank={special ? Rank.Challenger : quantityBorderToRank(quantity)}
-            url={url}
-            username={username}
-            avatarUrl={avatar}
-          />
-        ))}
+      <div className={styles['input-search-container']}>
+        <LensSvg
+          className={styles['icon-search']}
+          color={isTouched ? 'var(--ui-kit-secondary-400)' : 'white'}
+          size={26}
+        />
+        <InputOutline
+          className={styles['input-search']}
+          placeholder="Buscar por nombre"
+          onFocus={() => setIsTouched(true)}
+          onBlur={() => setIsTouched(false)}
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      {borders?.length === 0 && (
+        <div className={styles['not-found-container']}>
+          <Typography
+            variant="h2"
+            family="primary"
+            color="white"
+            size="4xl"
+            weight="extrabold"
+          >
+            No se han encontrado bordes con el nombre ingresado. Intenta con
+            otro.
+          </Typography>
+        </div>
+      )}
+
+      <ul>
+        {borders
+          ?.sort(callbackSort)
+          ?.map(({ id, url, special, quantity, username, avatar, name }) => (
+            <li key={id}>
+              <BorderRank
+                key={id}
+                name={name}
+                rank={
+                  special ? Rank.Challenger : quantityBorderToRank(quantity)
+                }
+                url={url}
+                username={username}
+                avatarUrl={avatar}
+              />
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
