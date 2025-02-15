456e4b6eb83c15aaa8cf00bdab100f66dc12c82e
GountzJs <tomasjsdev@gmail.com>
2025-02-15 02:09:34 -0300

    fix(web-rewards): disable cache vite

--- apps/web-rewards/vite.config.ts
+++ apps/web-rewards/vite.config.ts
@@ -1,51 +1,51 @@
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern:
-              /\/.*\.(?:js|css|html|png|jpg|jpeg|svg|ico|woff2?|ttf|eot|json)$/,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'static-assets',
-              expiration: {
-                maxEntries: 100,
-                maxAgeSeconds: 60 * 60 * 24 * 30,
-              },
-            },
-          },
-          {
-            urlPattern: /https:\/\/embee-org\.github\.io\//,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'cdn-assets',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24,
-              },
-            },
-          },
-        ],
-      },
-      manifest: {
-        name: 'Web Rewards App',
-        short_name: 'rewards_app',
-        description: 'Web Rewards app embeejayz',
-        start_url: '.',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-      },
-    }),
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   workbox: {
+    //     runtimeCaching: [
+    //       {
+    //         urlPattern:
+    //           /\/.*\.(?:js|css|html|png|jpg|jpeg|svg|ico|woff2?|ttf|eot|json)$/,
+    //         handler: 'CacheFirst',
+    //         options: {
+    //           cacheName: 'static-assets',
+    //           expiration: {
+    //             maxEntries: 100,
+    //             maxAgeSeconds: 60 * 60 * 24 * 30,
+    //           },
+    //         },
+    //       },
+    //       {
+    //         urlPattern: /https:\/\/embee-org\.github\.io\//,
+    //         handler: 'CacheFirst',
+    //         options: {
+    //           cacheName: 'cdn-assets',
+    //           expiration: {
+    //             maxEntries: 10,
+    //             maxAgeSeconds: 60 * 60 * 24,
+    //           },
+    //         },
+    //       },
+    //     ],
+    //   },
+    //   manifest: {
+    //     name: 'Web Rewards App',
+    //     short_name: 'rewards_app',
+    //     description: 'Web Rewards app embeejayz',
+    //     start_url: '.',
+    //     display: 'standalone',
+    //     background_color: '#ffffff',
+    //     theme_color: '#000000',
+    //   },
+    // }),
   ],
 
   server: {
