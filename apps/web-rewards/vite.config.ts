f2b9e473b8a82d3ccc88da19a6790445dee40e93
GountzJs <tomasjsdev@gmail.com>
2025-02-16 01:44:33 -0300

    fix(web-rewards): remove service worker

--- apps/web-rewards/vite.config.ts
+++ apps/web-rewards/vite.config.ts
@@ -1,56 +1,10 @@
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      srcDir: 'src',
-      filename: 'service-worker.js',
-      workbox: {
-        cleanupOutdatedCaches: true,
-        runtimeCaching: [
-          {
-            urlPattern:
-              /\/.*\.(?:js|css|html|png|jpg|jpeg|svg|ico|woff2?|ttf|eot|json)$/,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'static-assets',
-              expiration: {
-                maxEntries: 100,
-                maxAgeSeconds: 60 * 60, // 1 hora
-              },
-            },
-          },
-          {
-            urlPattern: /https:\/\/embee-org\.github\.io\//,
-            handler: 'StaleWhileRevalidate',
-            options: {
-              cacheName: 'cdn-assets',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 30, // 30 minutos
-              },
-            },
-          },
-        ],
-      },
-      manifest: {
-        name: 'Web Rewards App',
-        short_name: 'rewards_app',
-        description: 'Web Rewards app embeejayz',
-        start_url: '.',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-      },
-    }),
-  ],
-
+  plugins: [react()],
   server: {
     port: 3000,
   },
