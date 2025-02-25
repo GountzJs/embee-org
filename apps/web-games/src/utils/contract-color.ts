1aa91c3a273aaac5a8cb18ab6413743e3ca5e01a
GountzJs <tomasjsdev@gmail.com>
2025-02-25 15:16:10 -0300

    style(workspace): :art: execute prettier workspace

--- apps/web-games/src/utils/contract-color.ts
+++ apps/web-games/src/utils/contract-color.ts
@@ -1,20 +1,20 @@
 export function getContrastingColor(hex: string) {
-	if (hex.startsWith("#")) hex = hex.slice(1);
-	if (hex.length === 3) {
-		hex = hex
-			.split("")
-			.map((char) => char + char)
-			.join("");
-	}
-	const r = parseInt(hex.slice(0, 2), 16) / 255;
-	const g = parseInt(hex.slice(2, 4), 16) / 255;
-	const b = parseInt(hex.slice(4, 6), 16) / 255;
-	const luminance = (channel: any) =>
-		channel <= 0.03928
-			? channel / 12.92
-			: Math.pow((channel + 0.055) / 1.055, 2.4);
+  if (hex.startsWith('#')) hex = hex.slice(1);
+  if (hex.length === 3) {
+    hex = hex
+      .split('')
+      .map((char) => char + char)
+      .join('');
+  }
+  const r = parseInt(hex.slice(0, 2), 16) / 255;
+  const g = parseInt(hex.slice(2, 4), 16) / 255;
+  const b = parseInt(hex.slice(4, 6), 16) / 255;
+  const luminance = (channel: any) =>
+    channel <= 0.03928
+      ? channel / 12.92
+      : Math.pow((channel + 0.055) / 1.055, 2.4);
 
-	const l =
-		0.2126 * luminance(r) + 0.7152 * luminance(g) + 0.0722 * luminance(b);
-	return l > 0.5 ? "#000000" : "#FFFFFF";
+  const l =
+    0.2126 * luminance(r) + 0.7152 * luminance(g) + 0.0722 * luminance(b);
+  return l > 0.5 ? '#000000' : '#FFFFFF';
 }
--- apps/web-games/src/utils/format-time.ts
+++ apps/web-games/src/utils/format-time.ts
@@ -1,11 +1,11 @@
 export function formatTime(seconds: number): string {
-	// Calcular minutos y segundos
-	const minutes = Math.floor(seconds / 60);
-	const remainingSeconds = seconds % 60;
+  // Calcular minutos y segundos
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
 
-	// Formatear con dos dígitos (padStart)
-	const formattedMinutes = minutes.toString().padStart(2, "0");
-	const formattedSeconds = remainingSeconds.toString().padStart(2, "0");
+  // Formatear con dos dígitos (padStart)
+  const formattedMinutes = minutes.toString().padStart(2, '0');
+  const formattedSeconds = remainingSeconds.toString().padStart(2, '0');
 
-	return `${formattedMinutes}:${formattedSeconds}`;
+  return `${formattedMinutes}:${formattedSeconds}`;
 }
