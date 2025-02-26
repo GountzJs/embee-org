2a51c96d99743c1f8a6ff1a8dc8d5013b5a8e99a
GountzJs <tomasjsdev@gmail.com>
2025-02-26 14:35:03 -0300

    feat(web-rewards): :fire: disable page mantainer

--- apps/web-rewards/src/app/page.tsx
+++ apps/web-rewards/src/app/page.tsx
@@ -1,23 +1,26 @@
-import { BorderDetail } from '@/app/_sections/borders-detail/borders-detail';
 import { MainPage } from '@/shared/components/main-page/main-page';
-import { Typography } from '@embeeorg/ui-kit';
-import { Presentation } from './_sections/presentation/presentation';
 import styles from './page.module.css';
 
 export default function HomePage() {
   return (
     <MainPage className={styles['main-container']}>
-      <Presentation />
-      <Typography
-        variant="h2"
-        family="primary"
-        color="white"
-        size="4xl"
-        weight="extrabold"
-      >
-        Detalles de las recompensa
-      </Typography>
-      <BorderDetail />
+      <img
+        src="/chimney-energy.svg"
+        alt="Alert mainteiner"
+        width={400}
+        height={400}
+      />
+      <h1 className={styles['title-page']}>La web está en mantenimiento</h1>
+      <p className={styles['description-page']}>
+        Estamos trabajando en la nueva versión de la web para mejorar su
+        experiencia. Agradecemos su paciencia. 😊
+      </p>
+      <p className={styles['disclaimer']}>
+        <strong>Aviso:</strong> Durante este periodo de mantenimiento, algunas
+        funciones pueden estar inactivas. No nos hacemos responsables de
+        posibles pérdidas de datos o errores derivados del acceso parcial a la
+        web.
+      </p>
     </MainPage>
   );
 }
