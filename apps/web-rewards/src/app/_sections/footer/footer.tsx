e5aec7db37dd585a0beb2f34a5b172b9ffc9ba01
GountzJs <tomasjsdev@gmail.com>
2025-02-26 16:31:51 -0300

    Revert "feat(web-rewards): :fire: disable page mantainer"
    This reverts commit 2a51c96d99743c1f8a6ff1a8dc8d5013b5a8e99a.

--- apps/web-rewards/src/app/_sections/footer/footer.tsx
+++ apps/web-rewards/src/app/_sections/footer/footer.tsx
@@ -1,10 +1,113 @@
+import { Collaborator } from '@/shared/components/collaborator/collaborator';
+import { EmbeeLogo } from '@/shared/logos/embee-logo';
+import { Typography } from '@embeeorg/ui-kit';
+import { Link } from 'react-router';
 import styles from './footer.module.css';
 
 export function Footer() {
   return (
     <footer className={styles.container}>
-      <div className={styles.detail}></div>
-      <div className={styles['collaborators-container']}></div>
+      <div className={styles.detail}>
+        <Typography
+          variant="p"
+          family="secondary"
+          color="secondary-light"
+          size="base"
+          weight="semibold"
+          style={{
+            textShadow: '0 0 10px #000',
+          }}
+        >
+          Inspirado en{' '}
+          <Link
+            to="https://manz.dev/manzcards/"
+            target="_blank"
+            style={{
+              textDecoration: 'none',
+              fontWeight: 700,
+              color: 'var(--ui-kit-neutral-200)',
+            }}
+          >
+            Manzdev
+          </Link>{' '}
+          y{' '}
+          <Link
+            to="https://cards.uxanarangel.com/"
+            target="_blank"
+            className={styles['nav-link']}
+          >
+            AnaRangel
+          </Link>
+          .
+        </Typography>
+        <Typography
+          variant="p"
+          family="secondary"
+          color="secondary-light"
+          size="base"
+          weight="semibold"
+          style={{
+            textShadow: '0 0 10px #000',
+          }}
+        >
+          Mención honorífica a{' '}
+          <Link
+            to="https://uxcorprangel.github.io/"
+            target="_blank"
+            className={styles['nav-link']}
+          >
+            UX Corp Rangel
+          </Link>{' '}
+          por su increíble trabajo e inspiración dentro de la comunidad.
+        </Typography>
+
+        <Typography
+          variant="p"
+          family="secondary"
+          color="secondary-light"
+          size="base"
+          weight="regular"
+          style={{
+            textShadow: '0 0 10px #000',
+          }}
+        >
+          ¡Síguelos en sus redes y estén atentos a futuros proyectos! ¡Muchas
+          gracias!
+        </Typography>
+      </div>
+      <Link
+        to="https://links.embeejayz.com/"
+        target="_blank"
+        className={`${styles['nav-link']} ${styles['nav-embee']}`}
+      >
+        <EmbeeLogo /> Embeejayz
+      </Link>
+      <div className={styles['collaborators-container']}>
+        <Typography
+          variant="p"
+          family="secondary"
+          color="secondary-light"
+          size="xl"
+          weight="semibold"
+          style={{
+            textShadow: '0 0 10px #000',
+          }}
+        >
+          Colaboradores:
+        </Typography>
+        <div className={styles['collaborators']}>
+          <Collaborator
+            username="GountzJs"
+            url="https://github.com/GountzJs"
+            imgUrl="gountz"
+          />
+          <Collaborator
+            username="Su1zide"
+            url="https://www.instagram.com/su1zide/"
+            imgUrl="su1zide"
+          />
+        </div>
+      </div>
     </footer>
   );
 }
--- apps/web-rewards/src/app/page.tsx
+++ apps/web-rewards/src/app/page.tsx
@@ -1,26 +1,23 @@
+import { BorderDetail } from '@/app/_sections/borders-detail/borders-detail';
 import { MainPage } from '@/shared/components/main-page/main-page';
+import { Typography } from '@embeeorg/ui-kit';
+import { Presentation } from './_sections/presentation/presentation';
 import styles from './page.module.css';
 
 export default function HomePage() {
   return (
     <MainPage className={styles['main-container']}>
-      <img
-        src="/chimney-energy.svg"
-        alt="Alert mainteiner"
-        width={400}
-        height={400}
-      />
-      <h1 className={styles['title-page']}>La web está en mantenimiento</h1>
-      <p className={styles['description-page']}>
-        Estamos trabajando en la nueva versión de la web para mejorar su
-        experiencia. Agradecemos su paciencia. 😊
-      </p>
-      <p className={styles['disclaimer']}>
-        <strong>Aviso:</strong> Durante este periodo de mantenimiento, algunas
-        funciones pueden estar inactivas. No nos hacemos responsables de
-        posibles pérdidas de datos o errores derivados del acceso parcial a la
-        web.
-      </p>
+      <Presentation />
+      <Typography
+        variant="h2"
+        family="primary"
+        color="white"
+        size="4xl"
+        weight="extrabold"
+      >
+        Detalles de las recompensa
+      </Typography>
+      <BorderDetail />
     </MainPage>
   );
 }
