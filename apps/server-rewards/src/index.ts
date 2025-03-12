5f6511af669fc8a9bc6c104ca62a5b1efeffef78
GountzJs <tomasjsdev@gmail.com>
2025-03-12 04:51:52 -0300

    feat(server-rewards): :sparkles: add controller userscards

--- apps/server-rewards/src/index.ts
+++ apps/server-rewards/src/index.ts
@@ -1,6 +1,7 @@
 import { cors } from 'hono/cors';
 import { app } from './app';
 import { origins } from './consts/origin';
+import { UsersCardsController } from './controllers/users-cards.controller';
 import { UsersBordersRoutes } from './routes/users-borders.routes';
 import { UsersRoutes } from './routes/users.routes';
 
@@ -9,5 +10,7 @@ app.use('/api/*', cors({ origin: origins }));
 new UsersRoutes();
 
 new UsersBordersRoutes();
+
+new UsersCardsController();
 
 export default app;
