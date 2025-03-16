a3f724aa0c31958029ceb7f7f3f214234ba08536
GountzJs <tomasjsdev@gmail.com>
2025-03-16 12:57:18 -0300

    fix(web-games): :bug: users staff not ranking

--- apps/server-rewards/src/repositories/users.repository.ts
+++ apps/server-rewards/src/repositories/users.repository.ts
@@ -3,7 +3,7 @@ import { Client } from '@libsql/client';
 export class UsersRepository {
   async getById(client: Client, id: string) {
     const query = `
-      WITH RankedUser AS (
+      WITH NonStaffRanked AS (
         SELECT
           u.id,
           u.login AS username,
@@ -12,24 +12,44 @@ export class UsersRepository {
           RANK() OVER (ORDER BY COUNT(ub.id) DESC, u.login ASC) AS rank_position
         FROM users u
         LEFT JOIN user_borders ub ON u.id = ub.user_id
+        WHERE u.is_staff = false
+        GROUP BY u.id
+      ),
+
+      StaffUsers AS (
+        SELECT
+          u.id,
+          u.login AS username,
+          u.profile_image_url AS avatar,
+          COUNT(ub.id) AS quantityBorders,
+          NULL AS rank_position
+        FROM users u
+        LEFT JOIN user_borders ub ON u.id = ub.user_id
+        WHERE u.is_staff = true
         GROUP BY u.id
       )
+
       SELECT
-          id,
-          username,
-          avatar,
-          quantityBorders,
-          CASE
-            WHEN rank_position = 1 THEN 'CHALLENGER'
-            WHEN rank_position = 2 THEN 'MASTER'
-            WHEN rank_position = 3 THEN 'DIAMOND'
-            WHEN rank_position = 4 THEN 'PLATINIUM'
-            WHEN rank_position = 5 THEN 'GOLD'
-            WHEN rank_position = 6 THEN 'SILVER'
-            WHEN rank_position = 7 THEN 'BRONZE'
-            ELSE 'UNRANKED'
-          END AS rank
-      FROM RankedUser
+        id,
+        username,
+        avatar,
+        quantityBorders,
+        CASE
+          WHEN is_staff = true THEN 'CHALLENGER'
+          WHEN rank_position = 1 THEN 'CHALLENGER'
+          WHEN rank_position = 2 THEN 'MASTER'
+          WHEN rank_position = 3 THEN 'DIAMOND'
+          WHEN rank_position = 4 THEN 'PLATINIUM'
+          WHEN rank_position = 5 THEN 'GOLD'
+          WHEN rank_position = 6 THEN 'SILVER'
+          WHEN rank_position = 7 THEN 'BRONZE'
+          ELSE 'UNRANKED'
+        END AS rank
+      FROM (
+        SELECT *, true as is_staff FROM StaffUsers
+        UNION ALL
+        SELECT *, false as is_staff FROM NonStaffRanked
+      )
       WHERE id = ?
     `;
     const { rows } = await client.execute({
